5682d71cefc86f4d1803759089dd4dc56159ec6a
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-17 04:06:27 +0900

    test: proxy와 transaction 적용 확인을 위한 테스트 작성

---
+++ src/test/java/hello/springtx/apply/TxBasocTest.java
@@ -0,0 +1,59 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class TxBasocTest {
+
+    @Autowired
+    BasicService basicService;
+
+    @Test
+    void proxyCheck() {
+        log.info("aop class = {}", basicService.getClass());
+        Assertions.assertThat(AopUtils.isAopProxy(basicService)).isTrue();
+    }
+    // 프록시 여부 체크
+    // 이 프록시가 서비스를 참조해서 작동하게 되는 것이다.
+
+    @Test
+    void txTest() {
+        basicService.tx();
+        basicService.nonTx();
+    }
+
+    @TestConfiguration
+    static class TxApplyBasicConfig {
+        @Bean
+        BasicService basicService() {
+            return new BasicService();
+        }
+    }
+
+    @Slf4j
+    static class BasicService {
+
+        @Transactional
+        public void tx() {
+            log.info("call tx");
+            boolean txActive = TransactionSynchronizationManager.isSynchronizationActive();
+            log.info("tx active={}", txActive);
+        }
+
+        public void nonTx() {
+            log.info("call nonTx");
+            boolean txActive = TransactionSynchronizationManager.isSynchronizationActive();
+            log.info("tx active={}", txActive);
+        }
+    }
+}
