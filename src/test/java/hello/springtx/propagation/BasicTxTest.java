65232423633db4dfb3c16f13309af990d79e1c76
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-25 00:56:54 +0900

    test: 내부트랜잭션 롤백, 외부 트랜잭션 커밋시 반응 테스트

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -9,9 +9,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 
 import javax.sql.DataSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @Slf4j
 @SpringBootTest
@@ -103,8 +106,25 @@ public class BasicTxTest {
         log.info("내부 트랜잭션 커밋");
         txManager.commit(inner);
 
-        log.info("외부 트랜잭션 커밋");
+        log.info("외부 트랜잭션 롤백");
         txManager.rollback(outer);
     }
-}
 
+    @Test
+    void inner_rollback() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("내부 트랜잭션 롤백");
+        txManager.rollback(inner);
+        //rollback-only 표시함 (이를 통해 외부 트랜잭션이 커밋을 하더라도 최종적으로 롤백으로 처리함)
+
+        log.info("외부 트랜잭션 커밋");
+        assertThatThrownBy(() -> txManager.commit(outer))
+                .isInstanceOf(UnexpectedRollbackException.class);
+        //내부에서 롤백이 발생하여 최종적으로 커밋에서 롤백으로 바뀌게 되면 UnexpectedRollbackException 이 발생하게 된다.
+        //즉, 외부에서 커밋하려 하는데 rollback-only가 true일 경우 UnexpectedRollbackException을 발생시키는 것이다.
+    }
+}
